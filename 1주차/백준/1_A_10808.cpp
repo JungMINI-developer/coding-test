077cfe26fc5cb9f78f8e6a59cf9d079daf3f421f
jungmin <kjmhs501@gmail.com>
2026-04-04 23:28:43 +0900

    1주차: 백준 10808 문제 풀이

---
+++ 1주차/백준/1_A_10808.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+string str;
+int cnt[26];
+
+int main() {
+  cin >> str;
+  for (char a : str) {
+    cnt[a - 'a']++;
+  }
+
+  for (int i = 0; i < 26; i++) {
+    cout << cnt[i] << " ";
+  }
+  cout << "\n";
+}
--- 1주차/백준/1_A_2309.cpp
+++ 1주차/백준/1_A_2309.cpp
@@ -1,26 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
+// 순열은 사용하는 방법
+//  int main() {
+//    int b[9];
+//    int temp;
+//    for (int i = 0; i < 9; i++) {
+//      cin >> temp;
+//      b[i] = temp;
+//    }
+
+//   sort(b, b + 9);
+
+//   do {
+//     int sum = 0;
+//     for (int i = 0; i < 7; i++) {
+//       sum += b[i];
+//     }
+//     if (sum == 100) {
+//       break;
+//     }
+//   } while (next_permutation(b, b + 9));
+
+//   for (int i = 0; i < 7; i++) {
+//     cout << b[i] << "\n";
+//   }
+// }
+
+// 조합을 사용하는 방법
 int main() {
-  int b[9];
-  int temp;
+  int a[9], sum = 0;
+  vector<int> v;
+  pair<int, int> p;
+  // 배열 설정
   for (int i = 0; i < 9; i++) {
+    int temp;
     cin >> temp;
-    b[i] = temp;
+    a[i] = temp;
+    sum += temp;
   }
 
-  sort(b, b + 9);
-
-  do {
-    int sum = 0;
-    for (int i = 0; i < 7; i++) {
-      sum += b[i];
+  for (int i = 0; i < 9; i++) {
+    for (int j = i + 1; j < 9; j++) {
+      if (sum - a[i] - a[j] == 100) {
+        p = make_pair(a[i], a[j]);
+        break;
+      }
     }
-    if (sum == 100) {
-      break;
-    }
-  } while (next_permutation(b, b + 9));
+  }
 
-  for (int i = 0; i < 7; i++) {
-    cout << b[i] << "\n";
+  for (int i = 0; i < 9; i++) {
+    if (p.first == a[i] || p.second == a[i]) {
+      continue;
+    }
+    v.push_back(a[i]);
+  }
+
+  for (int i : v) {
+    cout << i << "\n";
   }
 }
