accbc7cf7aade31043f8961a2038eea95aa6b83a
jungmin <kjmhs501@gmail.com>
2026-04-06 21:42:57 +0900

    1주차: 1159 해설풀이

--- 1주차/백준/1_E_1159.cpp
+++ 1주차/백준/1_E_1159.cpp
@@ -1,41 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, cnt = 1;
-vector<string> v;
-string result = "";
+// 1. 나의 풀이
+//  int n, cnt = 1;
+//  vector<string> v;
+//  string result = "";
 
+// int main() {
+//   cin >> n;
+//   for (int i = 0; i < n; i++) {
+//     string temp;
+//     cin >> temp;
+//     v.push_back(temp);
+//   }
+
+//   sort(v.begin(), v.end());
+
+//   for (int i = 0; i < n - 1; i++) {
+//     // 현재 이름의 첫 글자와 다음 이름의 첫 글자가 같으면
+//     if (v[i][0] == v[i + 1][0]) {
+//       cnt++;
+//     } else {
+//       // 글자가 달라지는 시점에 개수 확인
+//       if (cnt >= 5) {
+//         result += v[i][0];
+//       }
+//       cnt = 1;  // 개수 초기화
+//     }
+//   }
+//   // 마지막 묶음 확인 (루프가 n-1에서 끝나므로 마지막 그룹 처리 필요)
+//   if (cnt >= 5) result += v[n - 1][0];
+
+//   // 결과 출력
+//   if (result.empty()) {
+//     cout << "PREDAJA" << "\n";
+//   } else {
+//     cout << result << "\n";
+//   }
+
+//   return 0;
+// }
+
+// 2. 해설 풀이
+
+int n, cnt[26];
+string s, ret;
 int main() {
   cin >> n;
   for (int i = 0; i < n; i++) {
-    string temp;
-    cin >> temp;
-    v.push_back(temp);
+    cin >> s;
+    cnt[s[0] - 'a']++;
   }
-
-  sort(v.begin(), v.end());
-
-  for (int i = 0; i < n - 1; i++) {
-    // 현재 이름의 첫 글자와 다음 이름의 첫 글자가 같으면
-    if (v[i][0] == v[i + 1][0]) {
-      cnt++;
-    } else {
-      // 글자가 달라지는 시점에 개수 확인
-      if (cnt >= 5) {
-        result += v[i][0];
-      }
-      cnt = 1;  // 개수 초기화
+  for (int i = 0; i < 26; i++) {
+    if (cnt[i] >= 5) {
+      ret += (i + 'a');
     }
   }
-  // 마지막 묶음 확인 (루프가 n-1에서 끝나므로 마지막 그룹 처리 필요)
-  if (cnt >= 5) result += v[n - 1][0];
 
-  // 결과 출력
-  if (result.empty()) {
-    cout << "PREDAJA" << "\n";
+  if (ret.size()) {
+    cout << ret << "\n";
   } else {
-    cout << result << "\n";
+    cout << "PREDAJA" << "\n";
   }
-
-  return 0;
 }
